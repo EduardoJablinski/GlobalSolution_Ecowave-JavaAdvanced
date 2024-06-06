7fdcad31ba38a3eac586fff1ccc805f26666aae3
EduardoJablinski <rm550975@fiap.com.br>
2024-06-06 17:56:45 -0300

    Commit

--- src/main/java/br/com/ecowave/service/ItensRecicladosService.java
+++ src/main/java/br/com/ecowave/service/ItensRecicladosService.java
@@ -4,7 +4,6 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
-
 import br.com.ecowave.model.ItensReciclados;
 import br.com.ecowave.repository.ItensRecicladosRepository;
 
